922bee5c577a5003828b9e51628cf0800e3955a7
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-27 09:30:40 -0700

    adding styling

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,8 +1,8 @@
 import './Animal.css';
 
-export default function Animal({ name, type, says }) {
+export default function Animal({ name, type, says, top, left }) {
   return (
-    <div className='animal-card'>
+    <div className='animal-card' style={{ position: 'absolute', top: 50px, left: 30% }}>
       <img src={`/images/${type}.svg`} />
       <span className='name'>{name}</span>
       <span>{type}</span>
